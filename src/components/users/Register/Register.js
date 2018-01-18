c7df9c5861d8095b423fa947edbadd7c0dac214c
KID <nakid001@gmail.com>
2018-01-19 00:56:47 +0700

    larger About size , fix signup cannot change color

--- src/components/users/Register/Register.js
+++ src/components/users/Register/Register.js
@@ -64,7 +64,7 @@ export class Register extends React.Component {
             <div className='space'></div>
             <input type="password" className="form-input" placeholder="Re enter password" style={{'width': '100%'}} name='REPass' onChange={this.handleChange}/>
           </div>
-          <button className='col-12 signupBtn' onClick={ () => { this.props.regisfire(this.props.user.username, this.props.user.password, this.props.user.repassword, this.props.user.mobile) } }><span className='signup--text'>Sign Up</span></button>
+          <button className='col-12 registerBtn' onClick={ () => { this.props.regisfire(this.props.user.username, this.props.user.password, this.props.user.repassword, this.props.user.mobile) } }><span className='signup--text'>Sign Up</span></button>
           <p className='col-12 signUpDivider'/>
           <div className='col-12'><p className='guideText'>Already have an account? <span><NavLink key='10' activeClassName='active' to='/login' id='signUpLink'>Log In</NavLink></span></p></div>
         </div>
