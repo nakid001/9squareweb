deff04afe075449e1f62d6c65cef0e1ab145c802
KID <nakid001@gmail.com>
2018-01-27 18:07:10 +0700

    decomment and delete unused code

--- NodeMcuCode/example/mqttconnection.js
+++ NodeMcuCode/example/mqttconnection.js
@@ -6,7 +6,6 @@ let mongodbClient = mongodb.MongoClient; //initialises the mongoDB client
 let mongodbURI = 'mongodb://127.0.0.1:27017/db'; //activating the MongoDB port 27017, here TempMontor is the name of the database
 let deviceRoot = "room1/temp"; //deviceroot is topic name given in arduino code 
 let collection,client; //initialise collection and client
-let mongoose = require('mongoose');
 let order = []
 let start = false
 let config = {
@@ -20,12 +19,12 @@ let config = {
 let a = firebase.initializeApp(config)
 getDataFirebase()
 mongodbClient.connect(mongodbURI, function(err, database) {
-//   if(err) throw err;
-//   db=database.db("mydb")
-//   collection=db.collection("real"); //name of the collection in the database
-//   client=mqtt.connect({ host: 'localhost', port: 1883 }); //connecting the mqtt server with the MongoDB database
-//   client.subscribe(deviceRoot); //subscribing to the topic name 
-//   client.on('message', insertEvent);
+  if(err) throw err;
+  db=database.db("mydb")
+  collection=db.collection("real"); //name of the collection in the database
+  client=mqtt.connect({ host: 'localhost', port: 1883 }); //connecting the mqtt server with the MongoDB database
+  client.subscribe(deviceRoot); //subscribing to the topic name 
+  client.on('message', insertEvent);
 }) 
 
 function insertEvent(topic,payload) {  
