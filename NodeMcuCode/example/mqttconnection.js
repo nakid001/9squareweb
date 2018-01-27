f796033715dd35bc6e32c5d485224ebd824cda32
KID <nakid001@gmail.com>
2018-01-27 18:06:28 +0700

    change data when firebase change

--- NodeMcuCode/example/mqttconnection.js
+++ NodeMcuCode/example/mqttconnection.js
@@ -7,39 +7,45 @@ let mongodbURI = 'mongodb://127.0.0.1:27017/db'; //activating the MongoDB port 2
 let deviceRoot = "room1/temp"; //deviceroot is topic name given in arduino code 
 let collection,client; //initialise collection and client
 let mongoose = require('mongoose');
+let order = []
+let start = false
 let config = {
-    apiKey: "AIzaSyBN4-YyLBgRJe-_ZuBe1OtpOYYXsw29P6c",
-    authDomain: "nafire-18969.firebaseapp.com",
-    databaseURL: "https://nafire-18969.firebaseio.com",
-    projectId: "nafire-18969",
-    storageBucket: "nafire-18969.appspot.com",
-    messagingSenderId: "463051690711"
+  apiKey: "AIzaSyBN4-YyLBgRJe-_ZuBe1OtpOYYXsw29P6c",
+  authDomain: "nafire-18969.firebaseapp.com",
+  databaseURL: "https://nafire-18969.firebaseio.com",
+  projectId: "nafire-18969",
+  storageBucket: "nafire-18969.appspot.com",
+  messagingSenderId: "463051690711"
 }
 let a = firebase.initializeApp(config)
-
+getDataFirebase()
 mongodbClient.connect(mongodbURI, function(err, database) {
-    if(err) throw err;
-   db=database.db("mydb")
-   collection=db.collection("real"); //name of the collection in the database
-   client=mqtt.connect({ host: 'localhost', port: 1883 }); //connecting the mqtt server with the MongoDB database
-   client.subscribe(deviceRoot); //subscribing to the topic name 
-   client.on('message', insertEvent);
-}); //connect the database with collecion
+//   if(err) throw err;
+//   db=database.db("mydb")
+//   collection=db.collection("real"); //name of the collection in the database
+//   client=mqtt.connect({ host: 'localhost', port: 1883 }); //connecting the mqtt server with the MongoDB database
+//   client.subscribe(deviceRoot); //subscribing to the topic name 
+//   client.on('message', insertEvent);
+}) 
 
-//function that displays the data in the MongoDataBase
 function insertEvent(topic,payload) {  
-    var key=topic.replace(deviceRoot,'');
-    // collection.update(  
-    //     { _id:key },
-    //     { $push: { events: { event: { value:payload, when:new Date() } } } },
-    //     { upsert:true },
-    //     function(err,docs) {
-    //       if(err) { console.log("Insert fail"); } // Improve error handling
-    //     }
-    // )
+  let key=topic.replace(deviceRoot,'');
+  firebase.database().ref('/mqtt').set({  // FOR FIREBASE
+    event: { value:payload.toString(), when:new Date() } })    
+  collection.insert({  //FOR MONGODB
+    events: payload.toString(),
+    when:new Date() 
+  }).then(() => {
+      console.log('Insert Complete')
+  })
+}
 
-    firebase.database().ref('/mqtt').set( { events: { event: { value:payload.toString(), when:new Date() } } })    
-    collection.insert({ events: payload.toString()}).then(() => {
-        console.log('Insert Complete')
-    })
+function getDataFirebase () {
+  firebase.database().ref('/rooms/room1/order/').on("value", function(snapshot) {
+    console.log("READING DATA")
+    console.log(snapshot.val())
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code)
+  });
+  
 }
