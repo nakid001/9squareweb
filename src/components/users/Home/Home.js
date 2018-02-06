16e9968f24203ce219b7e73cc4eba05a0c73f6fb
KID <nakid001@gmail.com>
2018-02-06 22:40:48 +0700

    fix title panel bug add slide temp done

--- src/components/users/Home/Home.js
+++ src/components/users/Home/Home.js
@@ -2,23 +2,83 @@ import React, { Component } from 'react'
 import './style.css'
 import img from './img/template2.jpg'
 import img2 from './img/template2.jpg'
+import bg1 from './img/a3aa4293d47b1d0e.jpg'
+import bg2 from './img/755791893f9b035.jpg'
+import bg3 from './img/47585378b3e74e1.jpg'
 import Modal from 'react-modal'
 import TestIcon from './img/start.png'
 import { Link } from 'react-router-dom'
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap'
 
+const items = [
+  {
+    src: bg1,
+
+    altText: '   ',
+    caption: '     เพียงเข้ารับการทดสอบของเรา แล้วเราจะช่วยคุณประมวลผลและแสดงข้อมูลของคุณอย่างรวดเร็ว โดยจะแสดงให้เห็นถึงความแม่นยำ ความรวดเร็ว ในการตอบสนองของคุณ และยังแสดงให้คุณเห็นถึง ranking ของคุณเทียบกับคนอื่นอีกด้วย'
+  },
+  {
+    src: bg2,
+    altText: 'Slide 2',
+    caption: 'Slide 2'
+  },
+  {
+    src: bg3,
+    altText: 'Slide 3',
+    caption: 'Slide 3'
+  }
+]
 // import { Link } from 'react-router'
 export class Home extends Component {
   constructor() {
     super()
  
     this.state = {
-      modalIsOpen: this.getCookie('username')
+      modalIsOpen: this.getCookie('username'),
+      activeIndex: 0 
     }
  
     this.openModal = this.openModal.bind(this)
     this.afterOpenModal = this.afterOpenModal.bind(this)
     this.closeModal = this.closeModal.bind(this)
     this.handlePress = this.handlePress.bind(this)    
+    this.getScroll = this.getScroll.bind(this)
+    this.next = this.next.bind(this)
+    this.previous = this.previous.bind(this)
+    this.goToIndex = this.goToIndex.bind(this)
+    this.onExiting = this.onExiting.bind(this)
+    this.onExited = this.onExited.bind(this)
+  }
+
+  onExiting() {
+    this.animating = true
+  }
+
+  onExited() {
+    this.animating = false
+  }
+
+  next() {
+    if (this.animating) return
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1
+    this.setState({ activeIndex: nextIndex })
+  }
+
+  previous() {
+    if (this.animating) return
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1
+    this.setState({ activeIndex: nextIndex })
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return
+    this.setState({ activeIndex: newIndex })
   }
   handlePress (event) {
     event.which = event.which || event.keyCode
@@ -44,16 +104,41 @@ export class Home extends Component {
       return parts.pop().split(';').shift()
     } else return ''
   }
+  getScroll () {
+    let sc = document.scrollingElement
+    console.log(sc.scrollTop)
+  }
   render() {
+    const { activeIndex } = this.state
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.src}
+        > 
+          <img src={item.src} alt={item.altText}  width="100%" height="470px"/>
+          <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+        </CarouselItem>
+      )
+    })
+
     return (
       <div id='wrapper'>
         <div id="header" className="title_content">ระบบจัดการทักษะกลไกการเคลื่อนไหว</div>
-        <div id="main-wrap" className="background_1">
-          <div id="content-wrap" className="box_content_background">
-            <div className="box_content">
-                เพียงเข้ารับการทดสอบของเรา แล้วเราจะช่วยคุณประมวลผลและแสดงข้อมูลของคุณอย่างรวดเร็ว โดยจะแสดงให้เห็นถึง
-                ความแม่นยำ ความรวดเร็ว ในการตอบสนองของคุณ และยังแสดงให้คุณเห็นถึง ranking ของคุณเทียบกับคนอื่นอีกด้วย
-            </div>
+        <div id="main-wrap" >
+          <div >
+
+            <Carousel
+              activeIndex={activeIndex}
+              next={this.next}
+              previous={this.previous}
+            >
+              <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+              {slides}
+              <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+              <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+            </Carousel>
           </div>
           <div>
             <Modal
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Main from './routes/main'
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
   <Main/>,
