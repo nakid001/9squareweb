0d26625d092af54b431f48019fad81adcbb02b14
killidiotd <nakid001@gmail.com>
2018-02-07 16:49:04 +0700

    delete apiKey

--- src/myfirebase.js
+++ src/myfirebase.js
@@ -1,6 +1,6 @@
 import * as firebase from 'firebase'
 let config = {
-  apiKey: 'AIzaSyBN4-YyLBgRJe-_ZuBe1OtpOYYXsw29P6c',
+  apiKey: '',
   authDomain: 'nafire-18969.firebaseapp.com',
   databaseURL: 'https://nafire-18969.firebaseio.com',
   projectId: 'nafire-18969',
