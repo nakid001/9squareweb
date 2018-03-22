f1331164b0c1f29cfe140c5ed5b3d2f72c8bf5df
Parut <nakid001@gmail.com>
2018-03-22 22:35:17 +0700

    move constructor from component to container in exLogin

--- src/containers/examiners/Login.js
+++ src/containers/examiners/Login.js
@@ -8,11 +8,38 @@ import { loginfire, logingmail } from '../../actions/examiner'
 //   LoginForm
 // } from '../components'
 class ExLoginContainer extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handlePress = this.handlePress.bind(this)
+  }
+
+  handleChange (event) {
+    if (event.target.name === 'User') {
+      this.props.inputlog(event.target.value, this.props.exam.password)
+    } else if (event.target.name === 'Pass') {
+      this.props.inputlog(this.props.exam.username, event.target.value)
+    }
+  }
+
+  handlePress (event) {
+    event.which = event.which || event.keyCode
+    if (event.which === 13) {
+      this.props.loginfire(this.props.exam.username, this.props.exam.password, 'examiner')
+    }
+  }
 
   render () {
     return (
       <div>
-        <Login {...this.props} />
+        <Login {...this.props} 
+          handleChange = {this.handleChange.bind(this)}
+          handlePress = {this.handlePress.bind(this)}
+        />
       </div>
     )
   }
