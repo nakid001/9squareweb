4bea724209fa9c79e34e5299ad06fcdd0f8feaa3
Parut <parut.singhapun@accenture.com>
2019-01-25 14:32:04 +0700

    Edit readme and remove comment

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,36 +1,14 @@
 import { combineReducers } from 'redux'
-// import mechanic from './mechanic'
-// import part from './part'
-// import garage from './garage'
-// // import user from './user'
-// import post from './post'
-// import auth from './auth'
-// import car from './car'
-// import deleted from './delete'
-// import service from './service'
+
 import userReducer from './user'
 import testReducer from './test'
 import ExReducer from './examiner'
-// import settings from './settings'
-// import payment from './payment'
-// import map from './map'
+
 
 const rootReducer = combineReducers({
-//   mechanic: mechanic,
-//   part: part,
-//   garage: garage,
   user: userReducer,
   test: testReducer,
   exam: ExReducer
-//   post: post,
-//   auth: auth,
-//   car: car,
-//   deleted: deleted,
-//   service: service,
-//   user: userReducer,
-//   settings: settings,
-//   payment: payment,
-//   map: map
 })
 
 export default rootReducer
