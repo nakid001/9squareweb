28c75b85b159a9186ecc3c621e7898f71644b803
Parut <nakid001@gmail.com>
2020-02-10 23:18:38 +0700

    resolve config

--- src/stores/stores.js
+++ src/stores/stores.js
@@ -2,8 +2,7 @@ import {createStore, applyMiddleware} from 'redux'
 import reducers from '../reducers'
 import thunk from 'redux-thunk'
 import { apiMiddleware } from 'redux-api-middleware'
-import initFirebase from './myfirebase'
-// import myfirebase from '../myfirebase.js'
+import initFirebase from '../myfirebase'
 
 initFirebase()
 const createStoreWithMiddleware = applyMiddleware(apiMiddleware)(createStore)
